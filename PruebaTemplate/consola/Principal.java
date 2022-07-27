dfa56dfc9b7cb280696f048421ea28186706a5df
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2022-07-27 22:27:37 +0000

    Probando diferentes clases

--- PruebaTemplate/consola/Principal.java
+++ PruebaTemplate/consola/Principal.java
@@ -1,5 +1,13 @@
-class Principal {
-  public static void main(String[] args) {
-    System.out.println("Hello world pi pi cucu!");
-  }
+//package consola;
+//import logica.*;
+
+public class Principal {
+
+	public static void main(String[] ar) {
+		
+		Alumno art1=new Alumno("Perez",2,20);
+		System.out.println("Los datos de art1 son:"+art1.toString());
+		
+	}
+
 }
